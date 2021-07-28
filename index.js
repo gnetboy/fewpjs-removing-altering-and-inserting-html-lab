eb2d4288bc240fbcb5ca06231b6319ccbd1bc129
gnetboy <gnetboy.y@gmail.com>
2021-07-27 21:11:28 -0700

    commit tree c0cb48c41ab4227ee142f6d5f96ca2ab4f5a75ed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+var newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML= "Gustavo is the champion!";
+newHeader.className = "victory";
